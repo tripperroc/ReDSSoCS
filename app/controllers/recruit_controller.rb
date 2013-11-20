e23bfc1855072554082736b8d29738e8c567f0eb
Christopher Homan <cmh@cs.rit.edu>
2013-11-20 11:32:48 -0500

    Added Randy's suggested changes.

--- app/controllers/recruit_controller.rb
+++ app/controllers/recruit_controller.rb
@@ -5,6 +5,7 @@ class RecruitController < ApplicationController
   def index   
     @invitation_url = url_for :controller => 'recruit', :action => 'invitation', :id => session[:recruiter_coupon]
     @email = session[:email_address]
+    @male_facebook_friends = session[:total_male_friends] 
     ResponderMailer.thank_you_email({:invitation_url => @invitation_url, :email_address => @email}).deliver
   end
 
